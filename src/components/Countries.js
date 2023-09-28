b7b5736e129897fa36c3bc7b1a2aad4a96368a11
ArthurGrenier <argnr@proton.me>
2023-09-28 15:13:01 +0200

    Push react app

---
+++ src/components/Countries.js
@@ -0,0 +1,68 @@
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from './Card';
+
+const Countries = () => {
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(12);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const [countrySearch, setCountrySearch] = useState([]);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const minRangeValue = "1";
+  const maxRangeValue = "250";
+
+  // Le useEffect se joue quand le components est monte
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, [])
+
+  return (
+    <div className='countries'>
+      <ul className='radio-container'>
+        <input
+          type="range"
+          min={minRangeValue}
+          max={maxRangeValue}
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.valueAsNumber)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              checked={continent === selectedRadio}
+              name='continentRadio'
+              onChange={(e) => setSelectedRadio(e.target.id)} />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+        <input className='searchButton' type="text" onChange={(e) => setCountrySearch([e.target.value])} />
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>Annuler la recherche</button>
+      )}
+      <ul>
+        {
+          data
+            .filter((country) => country.continents[0].includes(selectedRadio))
+            .filter((country) =>
+              country.translations.fra.common
+                .match(RegExp(countrySearch.map(term =>
+                  `(?=.*${term})`).join(''), 'i'))
+            )
+            .sort((a, b) => b.population - a.population)
+            .slice(0, rangeValue)
+            .map((country, index) =>
+              <Card key={index} country={country} />
+            )
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam earum ducimus et cupiditate tempora nam eum, tempore reiciendis ipsam perferendis. Accusamus, voluptate consequatur sed repellendus itaque maiores magnam adipisci, atque quam, vero ipsum possimus. Necessitatibus tempore deleniti doloremque, totam assumenda consequuntur modi quidem porro expedita odit vitae consequatur itaque similique aliquid quia quaerat, molestiae pariatur reiciendis! Provident molestias sapiente rerum illum nobis? Quia magnam, itaque aspernatur eius eos perferendis quo voluptatum voluptatem doloremque, sed excepturi veritatis minus voluptas commodi corporis explicabo enim omnis, totam culpa? Tempora, ducimus, cupiditate quas quasi enim maiores earum natus beatae repellendus, minima inventore debitis quae.</p>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quam! Hic facere dolorum corporis esse deleniti soluta rem tenetur et aspernatur cumque laboriosam unde odio eaque eveniet quidem minus perferendis eius nihil sequi, voluptatum harum similique vero fugit? Blanditiis rerum porro minima nulla qui beatae provident, et fugiat, ducimus commodi numquam eum corrupti? Adipisci labore fuga exercitationem tenetur, quibusdam voluptatibus quasi architecto magnam possimus nisi laborum voluptate odit quas beatae aut ab dolorum! Doloremque, corrupti dolor quae architecto praesentium sapiente consequatur illum exercitationem recusandae, molestiae animi eius illo? Labore, ipsam. Repudiandae amet doloremque necessitatibus laborum, praesentium voluptatem reprehenderit sint sequi magni alias distinctio velit fuga nulla eum iusto corrupti aliquid, consequatur, vitae ipsa impedit tenetur. Maiores autem ipsum consectetur, assumenda obcaecati nobis perspiciatis ab accusantium ducimus praesentium modi. Reiciendis est ut neque, maxime laboriosam quibusdam ullam nulla autem eligendi recusandae modi reprehenderit sint nisi possimus facere ex quaerat libero temporibus accusantium provident quos ratione distinctio consequuntur? Nulla minima aperiam repellat odit laudantium corrupti suscipit illo veniam cum consectetur illum voluptatibus, ab doloribus alias earum unde maiores nam rem sed, magnam quam beatae nostrum dignissimos dolores! Fugiat distinctio, laboriosam quibusdam rem eligendi harum illo at explicabo voluptas provident ipsam natus sit.</p>
+    </div>
+  );
+};
+
+export default About;
